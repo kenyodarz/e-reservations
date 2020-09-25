3230c97d368b400efacc9588108f61572396f62d
ByKenyoDarz <minajorge@hotmail.com>
2020-09-25 07:30:13 -0500

    Initial commit

---
+++ src/main/java/com/curso/ereservations/EReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.curso.ereservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EReservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EReservationsApplication.class, args);
+    }
+
+}
