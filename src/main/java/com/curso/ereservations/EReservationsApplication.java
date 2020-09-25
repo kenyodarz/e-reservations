cd5ffe30fc905514832e20bfb9ba1e09b524f1c9
ByKenyoDarz <minajorge@hotmail.com>
2020-09-25 13:05:46 -0500

    feat: Spring Security Basic Authentication Implemented

--- src/main/java/com/curso/ereservations/EReservationsApplication.java
+++ src/main/java/com/curso/ereservations/EReservationsApplication.java
@@ -2,8 +2,9 @@ package com.curso.ereservations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EReservationsApplication {
 
     public static void main(String[] args) {
