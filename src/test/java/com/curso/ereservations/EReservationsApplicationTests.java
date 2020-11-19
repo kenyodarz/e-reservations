3bd27b210891845debfc340521348cea52d70dec
Jorge Mina <minajorge@hotmail.com>
2020-11-19 10:27:32 -0500

    Create EReservationsApplicationTests.java

---
+++ src/test/java/com/curso/ereservations/EReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.ereservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EReservationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
